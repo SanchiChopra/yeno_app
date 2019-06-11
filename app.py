667519e1bc95e4d610e0699fe6011d9fa4557ffb
SanchiChopra <choprasanchi2@gmail.com>
2019-06-11 23:05:06 +0530

    Adding templates.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return render_template('index.html')
 
-@app.route("/harry")
+@app.route("/about")
 def harry():
-    return "Hello Harry!"
+    return render_template('about.html')
 
 app.run(debug=True)
+
+
