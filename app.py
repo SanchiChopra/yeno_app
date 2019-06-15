5a4fe03203c996f4e7d73bab039c75c9bf99d61f
SanchiChopra <choprasanchi2@gmail.com>
2019-06-15 23:29:28 +0530

    minor changes'

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/mywebsite'
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    '''
+    sno, name, email, phone_num, mes, date
+    '''
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    mes = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=False)
 
 @app.route("/")
 def home():
